2728f31188b37bff6f9d41b4e44f401c44c4fbc9
ChristineAshraf <97855069+ChristineAshraf@users.noreply.github.com>
2022-01-22 12:22:49 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,50 +1,72 @@
-<?php session_start();?>
+<?php
+session_start();
+if(isset($_SESSION['ID']))
+{
+if($_SESSION['Role']==1)
+{
+  include_once("client menu.php");
+}
+else if($_SESSION['Role']==2)
+{
+  include_once("admin menu.php");
+}
+else if($_SESSION['Role']==3)
+{
+  include_once("HRmenu.php");
+}
+else if($_SESSION['Role']==4)
+{
+include_once("auditor menu.php");
+}
+else
+{
+include_once("menu.php");
+}
+}
+ ?>
 <html>
 <head>
   <style>
-  .bg {
-          background-image: url("https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9");
-          height: 100%;
-          background-position: center;
-          background-repeat: no-repeat;
-          background-size: cover;
-        }
-
-          .sidenav {
-          height: 35px; /* Full-height: remove this if you want "auto" height */
-          width: 100%; /* Set the width of the sidebar */
-          z-index: 1; /* Stay on top */
-          top: 30; /* Stay at the top */
-          left: 0;
-          background-color: #111; /* Black */
-          overflow-x: hidden; /* Disable horizontal scroll */
-          padding-top: 20px;
-        }
-
-        /* The navigation menu links */
-        .sidenav a {
-          padding: 6px 8px 6px 16px;
-          text-decoration: none;
-          font-size: 15px;
-          color: #818181;
-          display: inline;
-        }
-
-        /* When you mouse over the navigation links, change their color */
-        .sidenav a:hover {
-          color: #f1f1f1;
-        }
-
-        /* Style page content */
-        .main {
-          margin-left: 160px; /* Same as the width of the sidebar */
-          padding: 0px 10px;
-        }
-        li
-        {
 
 
+        .wrapper {
+          padding: 50px;
         }
+
+        .image--cover {
+          width: 200px;
+          height: 200px;
+          border-radius: 50%;
+
+          object-fit: cover;
+          object-position: center ;
+        }
+
+        input[type=text],input[type=password], select {
+          width: 20%;
+          padding: 12px 20px;
+          margin: 8px 0;
+          display: inline-block;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          box-sizing: border-box;
+        }
+
+        input[type=submit] {
+          width: 20%;
+          background-color: #ff4500;
+          color: white;
+          padding: 14px 20px;
+          margin: 8px 0;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+          background-color: #9F2B00;
+        }
+
   </style>
 <header>
 
@@ -53,18 +75,6 @@
 
         <!-- Logo -->
 
-            <img src="Tectoy_simple_logo.png" width= "50px";height="50px"; >
-            <div class="sidenav">
-              <center>
-              <a href="HomePage.php">Home</a>
-             <a href="AboutUs.php">About Us</a>
-             <a href="OurProducts.php">Our Products</a>
-             <a href="#">Careers</a>
-             <a href="ContactUs.php">Contact Us</a>
-             <a href="SignUp.php">SignUp</a>
-             <a href="LoginPage.php">Login</a>
-           </center>
-            </div>
 
     <!-- Image menu in Header to contain an Image and
         a sample text over that image -->
@@ -72,26 +82,24 @@
 </header>
 
 </head>
-<?php
- echo"<img src='".$_SESSION['Profile']."' alt='profile pricture width='100' height='100'>";
- echo $_SESSION['Profile'];
- ?>
-
-
-<form method="post" action="" enctype="multipart/form-data">
+<?php echo"<center><div class='wrapper'><img class='image--cover' src=\"".$_SESSION['Profile']."\" alt='profile pricture width='100' height='100'><div></center>";?>
+<center>
+<form method="post" action="" enctype="multipart/form-data" >
   Upload Profile Picture<br>
-  <input type="file" name="profile" value=""><br>
+  <input type="file" name="profile" value="<?=$_SESSION['Profile']?>"><br>
   First Name<br>
   <input type="text" name="fname" value="<?=$_SESSION['FirstName']?>">  </br>
   Last Name<br>
   <input type="text" name="lname" value="<?=$_SESSION['LastName']?>"><br>
   Email<br>
   <input type="text" name="email" value="<?=$_SESSION['Email']?>"><br>
+
   Phone Number<br>
   <input type="text" name="phone" value="<?=$_SESSION['Phone']?>"><br>
   <input type="submit" name="submit">
 
 </form>
+</center>
 
  <?php
  $conn=mysqli_connect("localhost","root","","market place");
@@ -101,8 +109,8 @@
  }
  else if(isset($_POST['submit']))
  {
-if(!empty($_POST['profile']))
-{
+   if(!$_FILES['profile']['size'] == 0)
+   {
    $target_dir="images/";
    $target_file=$target_dir.basename($_FILES["profile"]["name"]);
    $uploadOK=1;
@@ -112,31 +120,31 @@ if(!empty($_POST['profile']))
    echo"Sorry,only JPG , JPEG & PNG files are allowed";
    $uploadOK=0;
    }
-   else if($uploadOK==0)
+   if($uploadOK==0)
    {
    echo"Sorry,your file was not uploaded!";
    }
-   else if($uploadOK!=0)
+   else
    {
-     if(move_uploaded_file($_FILES["profile"]["tmp_name"],$target_file))
-     {
-     echo"The file has been uploaded";
-     echo $target_file."hereeeee";
-     }
-     else
-     {
-     echo"Sorry,there was an error uploading your file";
-     }
+   if(move_uploaded_file($_FILES["profile"]["tmp_name"],$target_file))
+   {
+   echo"The file has been uploaded";
    }
-    $query="UPDATE user SET FirstName = \"".$_POST['fname']."\", LastName = \"".$_POST['lname']."\", Email = \"".$_POST['email']."\",Phone=\"".$_POST['phone']."\",ProfilePicture=\"".$target_file."\" WHERE UserID=\"".$_SESSION['ID']."\"";
-    
-    echo $query;
+   else
+   {
+   echo"Sorry,there was an error uploading your file";
+   }
+   }
+// echo"you did input a new profile picture <br>";
+// $variable=isset($_POST['profile']);
+// if($_FILES['profile']['size'] == 0)
+// {
+//
+//   echo"empetyyyyyyyyy";
+// }
+// echo $variable ."<br>";
 
-}
-else{
-  $query="UPDATE user SET FirstName = \"".$_POST['fname']."\", LastName = \"".$_POST['lname']."\", Email = \"".$_POST['email']."\",Phone=\"".$_POST['phone']."\" WHERE UserID=\"".$_SESSION['ID']."\"";}
-
-
+  $query="UPDATE user SET FirstName = \"".$_POST['fname']."\", LastName = \"".$_POST['lname']."\",Phone=\"".$_POST['phone']."\",ProfilePicture=\"".$target_file."\", Email = \"".$_POST['email']."\" WHERE UserID=\"".$_SESSION['ID']."\"";
  $result=$conn->query($query);
  if(!$result)
  {
@@ -145,11 +153,34 @@ else{
  else
  {
    echo"Data submited Successfully";
-   //header( "Location: Profile.php" );
+  // header( "Location:Profile.php" );
+
+
+ }
+}
+else
+{
+
+  // echo"you didn't change your profile picture<br>";
+  $query="UPDATE user SET FirstName = \"".$_POST['fname']."\", LastName = \"".$_POST['lname']."\",Phone=\"".$_POST['phone']."\", Email = \"".$_POST['email']."\" WHERE UserID=\"".$_SESSION['ID']."\"";
+ $result=$conn->query($query);
+ if(!$result)
+ {
+   echo"Coud no run the SQL Query";
+ }
+ else
+ {
+   echo"Data submited Successfully";
+    header("Location:Profile.php");
+
 
  }
 
- $retrive="select * from user  WHERE UserID=\"".$_SESSION['ID']."\"";
+}
+
+
+
+ $retrive="select * from user where UserID=".$_SESSION['ID'];
  $result1=$conn->query($retrive);
  if($retrive)
  {
@@ -160,6 +191,7 @@ else{
      $_SESSION['LastName']=$row['LastName'];
      $_SESSION['Email']=$row['Email'];
      $_SESSION['Phone']=$row['Phone'];
+     $_SESSION['ID']=$row['UserID'];
    }
  }
  else {
@@ -169,36 +201,35 @@ else{
  ?>
 
 
-
-<footer style="background-color:#ededed;position:absolute;bottom:0px;width:100%">
-  <br>
-  <div style="justify-content:space-evenly;display:flex">
-  <div>
-    <b>get to know us</b>
-    <ul>
-      <li>careers</li>
-      <li>blog</li>
-      <li>about amazon</li>
-    </ul>
-  </div>
-  <div>
-<b>Let us Help You </b>
-<ul>
- <a href="Account.php" alt=" your account"><li>your account</li></a>
- <a href="YouOrders.php"><li>you orders</li></a>
- <li>shipping rates & policies</li>
- <li>returns & replacemnts</li>
-</ul>
-</div>
-<div>
- <b>Make Money with Us</b>
- <ul>
-   <li>sell products on amazon</li>
-   <li>sell apps on amazon</li>
-     <li>Advertise Your Products</li>
+ <footer style="background-color:#ededed;width:100%;background-color:#263840;color:#818181;">
+   <br>
+   <div style="justify-content:space-evenly;display:flex">
+   <div>
+     <b>get to know us</b>
+     <ul>
+       <li>careers</li>
+       <li>blog</li>
+       <li>about amazon</li>
      </ul>
-</div>
+   </div>
+   <div>
+ <b>Let us Help You </b>
+ <ul>
+  <a href="Account.php" alt=" your account"><li>your account</li></a>
+  <a href="YouOrders.php"><li>you orders</li></a>
+  <li>shipping rates & policies</li>
+  <li>returns & replacemnts</li>
+ </ul>
+ </div>
+ <div>
+  <b>Make Money with Us</b>
+  <ul>
+    <li>sell products on amazon</li>
+    <li>sell apps on amazon</li>
+      <li>Advertise Your Products</li>
+      </ul>
+ </div>
 
-</div>
-</footer>
+ </div>
+ </footer>
 </html>
