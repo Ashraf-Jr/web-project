03c3f70224ad3736dda1dab8cab7b0e9e3b3d131
ChristineAshraf <97855069+ChristineAshraf@users.noreply.github.com>
2022-01-22 11:32:09 +0200

    Update Account.php

--- Account.php
+++ Account.php
@@ -1,86 +1,134 @@
 <html>
 <head>
+  <?php
+  session_start();
+  if(isset($_SESSION['ID']))
+  {
+  if($_SESSION['Role']==1)
+  {
+    include_once("client menu.php");
+  }
+  else if($_SESSION['Role']==2)
+  {
+    include_once("admin menu.php");
+  }
+  else if($_SESSION['Role']==3)
+  {
+    include_once("HRmenu.php");
+  }
+  else if($_SESSION['Role']==4)
+  {
+  include_once("auditor menu.php");
+  }
+  else
+  {
+  include_once("menu.php");
+  }
+  }
+   ?>
   <style>
-  .bg {
-          background-image: url("https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9");
-          height: 100%;
-          background-position: center;
-          background-repeat: no-repeat;
-          background-size: cover;
-        }
-
-          .sidenav {
-          height: 35px; /* Full-height: remove this if you want "auto" height */
-          width: 100%; /* Set the width of the sidebar */
-          z-index: 1; /* Stay on top */
-          top: 30; /* Stay at the top */
-          left: 0;
-          background-color: #111; /* Black */
-          overflow-x: hidden; /* Disable horizontal scroll */
-          padding-top: 20px;
-        }
-
-        /* The navigation menu links */
-        .sidenav a {
-          padding: 6px 8px 6px 16px;
-          text-decoration: none;
-          font-size: 15px;
-          color: #818181;
-          display: inline;
-        }
-
-        /* When you mouse over the navigation links, change their color */
-        .sidenav a:hover {
-          color: #f1f1f1;
-        }
 
         /* Style page content */
         .main {
           margin-left: 160px; /* Same as the width of the sidebar */
           padding: 0px 10px;
         }
-        li
+        a:hover
         {
-
-
+          color:white;
         }
+        a{
+          text-decoration: none;
+
+          color:#818181;
+        }
+
+/* <button class="button-58" role="button">Button 58</button> */
+
+/* CSS */
+.button-58 {
+  align-items: center;
+   background-color:non;
+  /* border: 2px solid #06f; */
+  /* box-sizing: border-box; */
+  cursor: pointer;
+  fill: #000;
+  font-family: Inter,sans-serif;
+  font-size: 20px;
+  font-weight: 600;
+  height: 48px;
+  justify-content: center;
+  letter-spacing: -.8px;
+  /* line-height: 24px; */
+  min-width: 140px;
+  /* outline: 0; */
+  padding: 0 17px;
+  text-align: center;
+  text-decoration: none;
+  transition: all .3s;
+
+}
+
+
+.button-58:hover {
+  background-color: #FF5349;
+  border-color: none;
+  /* fill: #06f; */
+}
+
+
+
+
+}
   </style>
 <header>
 
-    <!-- Top header menu containing
-        logo and Navigation bar -->
 
-        <!-- Logo -->
-
-            <img src="Tectoy_simple_logo.png" width= "50px";height="50px"; >
-            <div class="sidenav">
-              <center>
-              <a href="HomePage.php">Home</a>
-             <a href="AboutUs.php">About Us</a>
-             <a href="OurProducts.php">Our Products</a>
-             <a href="#">Careers</a>
-             <a href="ContactUs.php">Contact Us</a>
-             <a href="SignUp.php">SignUp</a>
-             <a href="LoginPage.php">Login</a>
-           </center>
-            </div>
-
-    <!-- Image menu in Header to contain an Image and
-        a sample text over that image -->
 
 </header>
 
 </head>
-<h1>My Account</h1>
+<center>
+<h1 style="color:#333333">My Account</h1>
+<center>
+<a href="#" ><button class="button-58"> Orders</a></button><br>
+<a href="chatClient.php"> <button class="button-58"> Inbox</li></a></button><br>
+  <a href="Profile.php"> <button class="button-58">Profile</li> </a></button><br>
+  <!-- <button class="button-58"><li>Wish List</li></button><br> -->
+</center>
 
-<ul >
-  <a href="#"><li>Your orders</li></a>
-  <li>Login & security</li>
-  <li>Your Payments</li>
-  <li>You Messages</li>
-  <a href="Profile.php"><li>Your Profile</li></a>
-  <li>Wish List</li>
+<footer class="footer" style="background-color:#263840;color:#818181;position:absolute;bottom:0px;width:100%">
+  <br>
+  <div style="justify-content:space-evenly;display:flex">
+  <div>
+    <b>get to know us</b>
+    <ul>
+      <li>careers</li>
+      <li>blog</li>
+      <li>about amazon</li>
+    </ul>
+  </div>
+  <div>
+<b>Let us Help You </b>
+<ul>
+ <a href="Account.php" alt=" your account"><li>your account</li></a>
+ <a href="YouOrders.php"><li>you orders</li></a>
+ <li>shipping rates & policies</li>
+ <li>returns & replacemnts</li>
+</ul>
+</div>
+<div>
+ <b>Make Money with Us</b>
+ <ul>
+   <li>sell products on amazon</li>
+   <li>sell apps on amazon</li>
+     <li>Advertise Your Products</li>
+     </ul>
+</div>
 
+</div>
+</footer>
 
 
 </html>
+
