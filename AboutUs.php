3253416ac9dfc5596ea5a77488bf045d479f06f0
ChristineAshraf <97855069+ChristineAshraf@users.noreply.github.com>
2022-01-22 11:29:55 +0200

    Update AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -1,88 +1,50 @@
+<?php
+session_start();
+if(isset($_SESSION['ID']))
+{
+if($_SESSION['Role']==1)
+{
+  include_once("client menu.php");
+}
+else if($_SESSION['Role']==2)
+{
+  include_once("admin menu.php");
+}
+else if($_SESSION['Role']==3)
+{
+  include_once("HRmenu.php");
+}
+else if($_SESSION['Role']==4)
+{
+include_once("auditor menu.php");
+}
+
+}
+else {
+  include_once("menu.php");
+
+}
+ ?>
 <html>
 <head>
 
     <style>
-    .sidenav {
-    height: 35px; /* Full-height: remove this if you want "auto" height */
-    width: 100%; /* Set the width of the sidebar */
-    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-    z-index: 1; /* Stay on top */
-    top: 30; /* Stay at the top */
-    left: 0;
-    background-color: #111; /* Black */
-    overflow-x: hidden; /* Disable horizontal scroll */
-    padding-top: 20px;
-  }
-
-  /* The navigation menu links */
-  .sidenav a {
-    padding: 6px 8px 6px 16px;
-    text-decoration: none;
-    font-size: 15px;
-    color: #818181;
-    display: inline;
-  }
-
-  /* When you mouse over the navigation links, change their color */
-  .sidenav a:hover {
-    color: #f1f1f1;
-  }
-
-  /* Style page content */
-  .main {
-    margin-left: 160px; /* Same as the width of the sidebar */
-    padding: 0px 10px;
-  }
-
-  /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
-  @media screen and (max-height: 450px) {
-    .sidenav {padding-top: 15px;}
-    .sidenav a {font-size: 18px;}
-  }
+    .margin
+    {
+      margin-left: 100px;
+      margin-right: 100px;
+    }
 
 
-
-    .bg {
-            background-image: url("https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9");
-            height: 100%;
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: cover;
-          }
     </style>
-    <body style="margin-left:100px;,margin-right:100px">
+    <body >
     <header>
-
-        <!-- Top header menu containing
-            logo and Navigation bar -->
-        <div id="top-header">
-
-            <!-- Logo -->
-            <div id="logo">
-              <a href="HomePage.php">  <img src="Tectoy_simple_logo.png" width= "50px";height="50px"; ></a>
-                <div class="sidenav">
-                  <center>
-                  <a href="HomePage.php">Home</a>
-                 <a href="AboutUs.php">About Us</a>
-                 <a href="OurProducts.php">Our Products</a>
-                 <a href="#">Careers</a>
-                 <a href="ContactUs.php">Contact Us</a>
-                 <a href="SignUp.php">SignUp</a>
-                 <a href="LoginPage.php">Login</a>
-               </center>
-                </div>
-
-        <!-- Image menu in Header to contain an Image and
-            a sample text over that image -->
-        <div id="header-image-menu">
-
-        </div>
     </header>
     <br><br><br>
 
 
     <center><h1>About US</h1></center>
-
+<div class="margin">
   <h3>WHO WE ARE</h3>
   <p>We'are sales performance agency.We've been helping businesses drive revenue with the use of inbound marketing and sales
     enablement tactics since 2012 .We've a proud HubSpot platinium and pride ourselves on using
@@ -97,16 +59,41 @@ inbound sales results because we've done it ourselves...
 to inbound marketing and found huge success after launching our sales strategy blog.Once we mastered
 the art of using though leadership content for lead generation ,we laubnched LeadG2 so we could help businesses do the exact same thing.
 </p>
+</div>
 
-    <footer style="background-color:#ededed;bottom:0px;position:absolute;width:98%">
-      <br>
-      Join The Community
 
-    <p>Author: Hege Refsnes</p>
-    <p><a href="mailto:maraime@example.com">hege@example.com</a></p>
-    follow us : facebook <br>
 
-    </footer>
+<footer style="background-color:#ededed;width:100%;background-color:#263840;color:#818181;position:absolute;bottom:0px">
+  <br>
+  <div style="justify-content:space-evenly;display:flex">
+  <div>
+    <b>get to know us</b>
+    <ul>
+      <li>careers</li>
+      <li>blog</li>
+      <li>about amazon</li>
+    </ul>
+  </div>
+  <div>
+<b>Let us Help You </b>
+<ul>
+ <a href="Account.php" alt=" your account"><li>your account</li></a>
+ <a href="YouOrders.php"><li>you orders</li></a>
+ <li>shipping rates & policies</li>
+ <li>returns & replacemnts</li>
+</ul>
+</div>
+<div>
+ <b>Make Money with Us</b>
+ <ul>
+   <li>sell products on amazon</li>
+   <li>sell apps on amazon</li>
+     <li>Advertise Your Products</li>
+     </ul>
+</div>
+
+</div>
+</footer>
   </body>
 
   </html>
