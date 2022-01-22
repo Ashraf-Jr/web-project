a166c65459a9b72c9395e3accc90cdcbe3a9b81e
Hady19H <96842348+Hady19H@users.noreply.github.com>
2022-01-22 11:48:05 +0200

    Update LoginPage.php

--- LoginPage.php
+++ LoginPage.php
@@ -1,38 +1,79 @@
-<?php session_start(); ?>
-<html>
+<?php
+session_start();
+if(isset($_SESSION['ID']))
+{
+if($_SESSION['Role']==1)
+{
+  include_once("client menu.php");
+}
+else if($_SESSION['Role']==2)
+{
+  include_once("admin menu.php");
+}
+else if($_SESSION['Role']==3)
+{
+  include_once("HRmenu.php");
+}
+else if($_SESSION['Role']==4)
+{
+include_once("auditor menu.php");
+}
+else
+{
+include_once("menu.php");
+}
+}
+ ?>
+ <html>
 <link rel="stylesheet" type="text/css" href="login.css" media="screen" />
 
 <?php
 if(isset($_SESSION['ID']))
 {
-  if($_SESSION['Role']==1)
-  {
-    include_once("client menu.php");
-  }
-  else if($_SESSION['Role']==2)
-  {
-    include_once("admin menu.php");
-  }
-  else if($_SESSION['Role']==3)
-  {
-    include_once("HRmenu.php");
-  }
-  else if($_SESSION['Role']==4)
-  {
-    include_once("auditormenu.php");
-  }
-  
+if($_SESSION['Role']==1)
+{
+  include_once("client menu.php");
+}
+else if($_SESSION['Role']==2)
+{
+  include_once("admin menu.php");
+}
+else if($_SESSION['Role']==4)
+{
+  include_once("auditor menu.php");
+}
 }
 else
 {
-    include_once("menu.php");  
+include_once("menu.php");
 }
-
  ?>
 <head>
   <style>
 
+  .wrapper {
+    padding: 50px;
+  }
 
+  .image--cover {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+
+    object-fit: cover;
+    object-position: center ;
+  }
+  .bg {
+          background-image: url("pexels-mitchell-luo-3694711.jpg");
+          height: 100%;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+        }
+        body
+        {
+          background-color: #d5d2cb;
+        }
 /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
 
 /* input[type=text]:focus {
@@ -53,40 +94,25 @@ input[type=button], input[type=submit], input[type=reset] {
   width:25%;
   height:60%;
 } */
-.wrapper {
-  padding: 100px;
-}
-
-.image--cover {
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-
-  object-fit: cover;
-  object-position: center right;
-}
-h4 
+form
 {
- color: #111; font-family: 'Helvetica Neue', sans-serif; font-size: 27px; font-weight: bold; letter-spacing: -1px; line-height: 1; text-align: center; 
+  background-color: transparent;
 }
-
-/*p
- {
-  color: #685206; font-family: 'Helvetica Neue', sans-serif; font-size: 14px; line-height: 24px; margin: 0 0 24px; text-align: justify; text-justify: inter-word;
-   }*/
-
-
-
   </style>
 </head>
+<body>
+<div class="">
 <header>
 
     <!-- Top header menu containing
         logo and Navigation bar -->
 
         <?php
+        $unknown="R.png";
+
         if(isset($_POST['submit']))
         {
+
         $conn=mysqli_connect("localhost","root","","market place");
         if(!$conn)
         {
@@ -114,10 +140,19 @@ h4
         //header("Location:HomePage.php");
         }
         else {
+          echo"<center>";
+          echo" <div class='wrapper'> <img class='image--cover'src='".$unknown."' alt='profile picture' width='100'height='100'></div>";
           echo"Invalid email or Password!<br>";
           echo"try again";
+          echo"</center>";
+
         }
 
+        }
+        else {
+          echo"<center>";
+          echo" <div class='wrapper'> <img class='image--cover'src='".$unknown."' alt='profile picture' width='100'height='100'></div>";
+          echo"</center>";
         }
 
 //echo $_SESSION['Profile'];
@@ -137,14 +172,14 @@ h4
 </center>
 <center>
   <div class="box">
-  <?php 
-  if(isset($_SESSION['Profile']))
-  {
-    echo"  <div class='wrapper'><img class='image--cover' src='".$_SESSION['Profile']."' alt='profile picture' width='100'height='100'></div>";
+      <?php
+    if(isset($_SESSION['Profile']))
+    {
+     echo" <div class='wrapper'> <img class='image--cover'src='".$_SESSION['Profile']."' alt='profile picture' width='100'height='100'></div>";
 
-  }
+    }
 
-  ?>
+    ?>
   <p style="width:40%">Welcome back! Log in to your account to view today's clients:</p>
 
   <form method="post" action="">
@@ -155,14 +190,14 @@ h4
   <!-- <img src="OIP (1).jfif" alt="lock"width="6%"height="6%"> -->
   <label for="Password">Password</label>
 
-    <input type="password" name="pass" value=""><br><br>
+    <input type="password" name="pass" value=""><br>
 
     <!-- <img src="OIP (2).jfif" alt="login"width="4%" height="3%"> -->
       <input type="submit" name="submit" value="login">
 
     </form>
-      <p>Forgot password?<a href=# > link</a></p>
-      create account <a href="SignUp.php">signUp</a>
+      <!-- <p>Forgot password?<a href=# > link</a></p> -->
+      create account <a href="SignUp.php">signUp</a><br><br>
 </center>
 
 </div>
@@ -177,4 +212,40 @@ h4
 follow us : facebook <br>
 
 </footer> -->
+
+ <footer class="footer" style="background-color:#263840;color:#818181">
+   <br>
+   <div style="justify-content:space-evenly;display:flex">
+   <div>
+     <b>get to know us</b>
+     <ul>
+       <li>careers</li>
+       <li>blog</li>
+       <li>about amazon</li>
+     </ul>
+   </div>
+   <div>
+<b>Let us Help You </b>
+<ul>
+  <a href="Account.php" alt=" your account"><li>your account</li></a>
+  <a href="YouOrders.php"><li>you orders</li></a>
+  <li>shipping rates & policies</li>
+  <li>returns & replacemnts</li>
+</ul>
+</div>
+<div>
+  <b>Make Money with Us</b>
+  <ul>
+    <li>sell products on amazon</li>
+    <li>sell apps on amazon</li>
+      <li>Advertise Your Products</li>
+      </ul>
+</div>
+
+</div>
+ </footer>
+
+
+</body>
+</div>
 </html>
